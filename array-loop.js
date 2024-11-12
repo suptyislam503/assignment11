1a5c20687d98f18996be7706be376c3fd7500ebc
suptyislam503 <kotaliparaulahati@gmail.com>
2024-11-12 19:19:19 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,16 @@
+var numbers = [1,3,5,7,9,11,13,15,17,19,21,23,25,26];
+for(var i = 0; i <= numbers.length; i++){
+ var element = numbers [i];
+ console.log(element);
+}
+ 
+
+for(var Mango = 0; Mango < 10; Mango++){
+    console.log("Mango is the sweetest fruit in Bangladesh");
+    console.log(Mango);
+}
+for (var Papaya = 0; Papaya <10; Papaya++) {
+    console.log("Papaya is a green fruit");
+    console.log(Papaya);
+    
+}
---
+++ if-else.js
@@ -0,0 +1,28 @@
+var Mango = 80;
+var Apple = 70;
+var Orange = 75;
+var Grape = 60;
+if(Mango < 90){
+    console.log("Mango is a sweet fruit");
+}
+else{
+    console.log("Mango is a summer fruit")
+}
+if(Apple < 65){
+    console.log("The taste of Apple is sweet")
+}
+else{
+    console.log("Apple is good for health")
+}
+if(Orange < 50){
+    console.log("Orange contains vitamin C")
+}
+else{
+    console.log("Orange has lot of benifit")
+}
+if(Grape < 45){
+    console.log("Grape grows a lot at a time")
+}
+else{
+    console.log("Grape is my favourite fruit")
+}
